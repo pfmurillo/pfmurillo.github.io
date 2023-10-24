d9ad5bb03caadc40f706dfe37381274e18780dae
Pierre-Frédéric Murillo <pierrefrederic.murillo@gmail.com>
2023-10-24 16:50:04 +0200

    Update: home page texts

--- src/lib/i18n/locales.js
+++ src/lib/i18n/locales.js
@@ -2,19 +2,20 @@ export const locales = {
 	en: {
 		'language.version': 'english version',
 		// global information
-		'global.title': 'Pierre, a Web Dev',
+		'global.title': 'Pierre Murillo',
 		// navigation items
 		'nav.portfolio': 'Portfolio',
 		'nav.contact': 'Contact',
 		// home
-		'home.title': 'Svelte • API REST • SQL • JWT • EVM',
-		'home.description': 'Pierre Murillo is a web developer',
+		'home.title': 'Full stack web Developer',
+		'home.description':
+			'Pierre Murillo is a full stack web developer: Svelte, API REST, SQL, JWT, EVM',
 		'home.h1': "I'm Pierre, a full stack web developer",
 		'home.h2': 'Svelte • API REST • SQL • JWT • EVM',
 		'home.p_work':
 			'My work is guided by a concern for <strong>details</strong>, compliance with <strong>standards</strong> and <strong>best practices</strong>, particularly in terms of accessibility, to make the web more <strong>inclusive</strong>.',
 		'home.p_front':
-			'I integrate sober and elegant <strong>mobile first</strong> interfaces using the latest technologies, in particular the <strong>Svelte</strong> framework, in which I specialise.',
+			'I integrate sober and elegant <strong>mobile first</strong> interfaces using the latest technologies, including the <strong>Svelte</strong> framework, in which I specialise.',
 		'home.p_steps':
 			"Whatever your project, I'm here to support you every step of the way: from the <strong>design</strong> and <strong>development</strong> to the <strong>hosting</strong> of your application.",
 		'home.p_contact': "If you have any questions, please don't hesitate to",
@@ -36,21 +37,22 @@ export const locales = {
 	fr: {
 		'language.version': 'version française',
 		// global information
-		'global.title': 'Pierre, Dev Web',
+		'global.title': 'Pierre Murillo',
 		// navigation items
 		'nav.portfolio': 'Portfolio',
 		'nav.contact': 'Contact',
 		// home
-		'home.title': 'Svelte • API REST • SQL • JWT • EVM',
-		'home.description': 'Pierre Murillo est développeur web',
+		'home.title': 'Développeur web full stack',
+		'home.description':
+			'Pierre Murillo est un développeur web full stack : Svelte, API REST, SQL, JWT, EVM',
 		'home.h1': 'Je suis Pierre, développeur web',
 		'home.h2': 'Svelte • API REST • SQL • JWT • EVM',
 		'home.p_work':
 			"Mon travail est guidé par le souci du <strong>détail</strong>, le respect des <strong>standards</strong> et des <strong>bonnes pratiques</strong>, notamment en terme d'accessibilité, pour un web plus <strong>inclusif</strong>.",
 		'home.p_front':
-			"J'intègre des interfaces <strong>mobile first</strong>, sobres et élégantes en utilisant les dernières technologies, notament le framework <strong>Svelte</strong>, dans lequel je me suis spécialisé.",
+			"J'intègre des interfaces <strong>mobile first</strong>, sobres et élégantes en utilisant les dernières technologies, dont le framework <strong>Svelte</strong>, dans lequel je me suis spécialisé.",
 		'home.p_steps':
-			"Quelque soit votre projet, je suis là pour vous accompagner à chaque étape : de la <strong>conception</strong> et du <strong>développement</strong> à l'<strong>hébergement</strong> de votre application. ",
+			"Quel que soit votre projet, je suis là pour vous accompagner à chaque étape : de la <strong>conception</strong> et du <strong>développement</strong> à l'<strong>hébergement</strong> de votre application. ",
 		'home.p_contact': 'Je suis à votre disposition si vous avez la moindre question :',
 		'home.contact': 'contactez-moi !',
 		// portfolio
